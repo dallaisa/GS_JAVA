ac91452ca416e1ed36ed1470ac530df9ab086478
isabelle Dallabeneta <isabelledallacarlesso.gmail.com>
2025-06-05 17:37:53 -0300

    concluido

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Digite nome do bioma:");
-        String nome = sc.nextLine();
+        System.out.println("Digite o nome do bioma:");
+        String nomeDigitado = sc.nextLine().trim().toLowerCase();
 
-        Bioma bioma = new Bioma(1, nome, "úmido", "aves tropicais", "mata fechada");
-        SentinelAnt ant = new SentinelAnt(101, -23.5, -46.6, 60.5, 35.0, 400, 1.8, bioma);
-        SentinelLadybug ladybug = new SentinelLadybug(202, -23.5, -46.6, "estalo", 70.0, bioma);
+        Bioma bioma = BiomaFactory.criarBioma(nomeDigitado);
+        SentinelAnt ant = BiomaFactory.criarAnt(nomeDigitado, bioma);
+        SentinelLadybug ladybug = BiomaFactory.criarLadybug(nomeDigitado, bioma);
 
         System.out.println(ant.diagnostico(true));
         System.out.println(ladybug.diagnostico());
 
-        Comunidade comunidade = new Comunidade(1, "Vila Verde", "Joana", "11 99999-0000", "Vale do Xingu", 320, bioma);
+        Comunidade comunidade = new Comunidade(1, "Vila Verde", "Joana", "11 99999-0000", "SP", 320, bioma);
         System.out.println(comunidade.getResumo());
 
         Alerta alerta = new Alerta(1, "ANT", 3, "Temperatura e CO2 elevados", ant);
---
+++ src/model/BiomaFactory.java
@@ -0,0 +1,61 @@
+package model;
+
+public class BiomaFactory {
+
+    public static Bioma criarBioma(String nome) {
+        switch (nome.toLowerCase()) {
+            case "amazônia":
+                return new Bioma(1, "Amazônia", "úmido", "biodiversidade alta", "floresta densa");
+            case "cerrado":
+                return new Bioma(2, "Cerrado", "tropical sazonal", "aves do planalto", "vegetação de galeria");
+            case "caatinga":
+                return new Bioma(3, "Caatinga", "semiárido", "animais adaptados à seca", "vegetação espinhosa");
+            case "pantanal":
+                return new Bioma(4, "Pantanal", "úmido com inundações", "aves aquáticas", "vegetação aquática");
+            case "mata atlântica":
+                return new Bioma(5, "Mata Atlântica", "úmido", "diversidade endêmica", "mata fechada");
+            case "pampa":
+                return new Bioma(6, "Pampa", "subtropical", "aves campestres", "campos e gramíneas");
+            default:
+                return new Bioma(0, nome, "desconhecido", "fauna genérica", "vegetação genérica");
+        }
+    }
+
+    public static SentinelAnt criarAnt(String nomeBioma, Bioma bioma) {
+        switch (nomeBioma.toLowerCase()) {
+            case "amazônia":
+                return new SentinelAnt(101, -3.1, -60.0, 75.0, 32.0, 600, 2.5, bioma);
+            case "cerrado":
+                return new SentinelAnt(102, -15.5, -47.5, 55.0, 33.0, 300, 1.8, bioma);
+            case "caatinga":
+                return new SentinelAnt(103, -9.4, -40.3, 40.0, 38.0, 250, 1.5, bioma);
+            case "pantanal":
+                return new SentinelAnt(104, -16.6, -56.1, 65.0, 30.0, 400, 2.0, bioma);
+            case "mata atlântica":
+                return new SentinelAnt(105, -23.5, -46.6, 70.0, 28.0, 350, 2.2, bioma);
+            case "pampa":
+                return new SentinelAnt(106, -30.0, -51.2, 50.0, 25.0, 200, 1.6, bioma);
+            default:
+                return new SentinelAnt(999, 0.0, 0.0, 0.0, 0.0, 100, 1.0, bioma);
+        }
+    }
+
+    public static SentinelLadybug criarLadybug(String nomeBioma, Bioma bioma) {
+        switch (nomeBioma.toLowerCase()) {
+            case "amazônia":
+                return new SentinelLadybug(201, -3.1, -60.0, "canto agudo", 90.0, bioma);
+            case "cerrado":
+                return new SentinelLadybug(202, -15.5, -47.5, "estridulação", 60.0, bioma);
+            case "caatinga":
+                return new SentinelLadybug(203, -9.4, -40.3, "estalido seco", 50.0, bioma);
+            case "pantanal":
+                return new SentinelLadybug(204, -16.6, -56.1, "chiado", 70.0, bioma);
+            case "mata atlântica":
+                return new SentinelLadybug(205, -23.5, -46.6, "zumbido baixo", 80.0, bioma);
+            case "pampa":
+                return new SentinelLadybug(206, -30.0, -51.2, "estalido suave", 55.0, bioma);
+            default:
+                return new SentinelLadybug(999, 0.0, 0.0, "som genérico", 40.0, bioma);
+        }
+    }
+}
